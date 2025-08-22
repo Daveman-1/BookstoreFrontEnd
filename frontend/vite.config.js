69b4521332c3232f45ddac68eb6713a6bc258759
Daveknb <dknberimah@gmail.com>
2025-08-22 00:39:56 +0000

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
+    host: true,                
+    port: process.env.PORT
   },
   build: {
     outDir: 'dist',
